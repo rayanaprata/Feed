0812fca7e53f6c13b77bd9803248d1ebd49df96d
Rayana Prata <50561896+rayanaprata@users.noreply.github.com>
2021-10-02 19:10:28 -0300

    validation to identify logged user and redirect directly to feed

--- Feed/Sources/AppDelegate.swift
+++ Feed/Sources/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseAuth
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,17 +18,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UITabBar.appearance().tintColor = UIColor(red: 249/255, green: 202/255, blue: 3/255, alpha: 1)
 
         FirebaseApp.configure()
+        setupRootViewController()
         
-        let viewController = SignInViewController()
+        return true
+    }
+    
+    private func setupRootViewController() {
+        var viewController: UIViewController = SignInViewController()
+        
+        if Auth.auth().currentUser != nil {
+            viewController = TabBarViewController()
+        }
+        
         let navigationController = UINavigationController(rootViewController: viewController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
         //window?.rootViewController = TabBarViewController()
         window?.makeKeyAndVisible()
-        
-        return true
     }
+
 
 }
 
