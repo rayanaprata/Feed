4fa7e58683f56f19f0adf730856074d0a84b2727
claudionoberto <83810561+claudionoberto@users.noreply.github.com>
2021-10-02 14:40:14 -0400

    Feed screen with name & adding changing tabbar selected item color

--- Feed/Sources/AppDelegate.swift
+++ Feed/Sources/AppDelegate.swift
@@ -14,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
+        UITabBar.appearance().tintColor = UIColor(red: 249/255, green: 202/255, blue: 3/255, alpha: 1)
+
         FirebaseApp.configure()
         
         //let viewController = SignInViewController()
